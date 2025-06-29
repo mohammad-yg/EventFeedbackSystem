cbf75dc77ea222d764363344e4c1c8ccbd2926c3
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 01:20:30 +0330

    feat(auth): add user entity

---
+++ src/EventFeedbackSystem.Core/EventFeedbackSystem.Core/Auth/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using EventFeedbackSystem.Core.Shared;
+
+namespace EventFeedbackSystem.Core.Auth.Entities;
+
+public class User : Entity<long>
+{
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+
+    public User(string email, string hashPasswords)
+    {
+        Email = email;
+        PasswordHash = hashPasswords;   
+    }
+}
