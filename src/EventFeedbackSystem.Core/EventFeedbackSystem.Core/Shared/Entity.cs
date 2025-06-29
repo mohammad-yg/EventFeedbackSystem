4db975223d497d346cbbc5cdcb22707724191204
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 01:20:13 +0330

    feat(core): add base entity

---
+++ src/EventFeedbackSystem.Core/EventFeedbackSystem.Core/Shared/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace EventFeedbackSystem.Core.Shared;
+
+public class Entity<TKey> where TKey : struct
+{
+    public required TKey Id { get; set; }
+    public required bool IsDeleted { get; set; } = false;
+
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity<TKey> other)
+            return false;
+
+        return Id.Equals(other.Id);
+    }
+}
