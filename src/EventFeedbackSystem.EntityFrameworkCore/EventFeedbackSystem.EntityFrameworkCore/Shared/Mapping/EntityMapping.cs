90db99b346c236516f55b2d759dd8c2371d357f4
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 04:20:30 +0330

    feat(auth): add Entity Mapping (database mapping)

---
+++ src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/Shared/Mapping/EntityMapping.cs
@@ -0,0 +1,33 @@
+﻿using EventFeedbackSystem.Core.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventFeedbackSystem.EntityFrameworkCore.Shared.Mapping;
+
+public class EntityMapping
+{
+    public static ModelBuilder ApplyMapping(ModelBuilder builder)
+    {
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            if (!typeof(Entity<>).IsAssignableFrom(entityType.ClrType.BaseType)) continue;
+
+            builder
+                .Entity(entityType.ClrType)
+                .HasKey(nameof(Entity<int>.Id));
+
+            //Id
+            builder
+                .Entity(entityType.ClrType)
+                .Property(nameof(Entity<int>.Id))
+                .IsRequired();
+
+            //IsDeleted
+            builder
+                .Entity(entityType.ClrType)
+                .Property(nameof(Entity<int>.IsDeleted))
+                .HasDefaultValue(false);
+        }
+
+        return builder;
+    }
+}
