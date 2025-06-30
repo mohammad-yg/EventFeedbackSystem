3ba00b81ad8017a5543f6967c6894ac02a0f498b
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 06:24:11 +0330

    feat(event): add event entity

---
+++ src/EventFeedbackSystem.Core/EventFeedbackSystem.Core/Events/Entities/Event.cs
@@ -0,0 +1,22 @@
+﻿namespace EventFeedbackSystem.Core.Events.Entities;
+
+public class Event : Entity<long>
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set; }
+    public string Location { get; set; }
+
+    public Event(string title, string description, DateTime dateTime, string location)
+    {
+        Title = title;
+        Description = description;
+        DateTime = dateTime;
+        Location = location;
+    }
+
+    //for add migration
+    public Event()
+    {
+    }
+}
---
+++ src/EventFeedbackSystem.Core/EventFeedbackSystem.Core/usings.cs
@@ -0,0 +1,1 @@
+﻿global using EventFeedbackSystem.Core.Shared;
