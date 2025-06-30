efbc7d11fe5277cd60cdeada680d2b669a37e0d6
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 07:21:36 +0330

    feat(Application): add some linq (query) extenstions

---
+++ src/EventFeedbackSystem.Application.Shared/EventFeedbackSystem.Application.Shared/Shared/Exctenstions/QueryExtenstions.cs
@@ -0,0 +1,23 @@
+﻿namespace EventFeedbackSystem.Application.Shared.Exctenstions;
+
+public static class QueryExtenstions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Func<IQueryable<T>, IQueryable<T>> predicate)
+    {
+        return condition ? predicate(query) : query;
+    }
+
+    public static IQueryable<T> OrderByIf<T>(this IQueryable<T> query, bool condition, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy)
+    {
+        return condition ? orderBy(query) : (IOrderedQueryable<T>)query;
+    }
+
+    public static IQueryable<T> SkipIf<T>(this IQueryable<T> query, bool condition, int count)
+    {
+        return condition ? query.Skip(count) : query;
+    }
+    public static async Task<List<T>> ToListAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
+    {
+        return await query.ToListAsync(cancellationToken);
+    }
+}
