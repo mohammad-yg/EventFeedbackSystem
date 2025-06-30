f93f232b3f406b8be7aefdc2657a6af66309afed
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 04:22:33 +0330

    feat(auth): add UserMapping (database mapping)

---
+++ src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/Auth/Mapping/UserMapping.cs
@@ -0,0 +1,15 @@
+﻿using EventFeedbackSystem.Core.Auth.Entities;
+
+namespace EventFeedbackSystem.EntityFrameworkCore.Auth.Mapping;
+
+public class UserMapping : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        //Email
+        builder.Property(e => e.Email).IsRequired().HasMaxLength(256);
+
+        //PasswordHash
+        builder.Property(e => e.PasswordHash).IsRequired().HasMaxLength(64);
+    }
+}
