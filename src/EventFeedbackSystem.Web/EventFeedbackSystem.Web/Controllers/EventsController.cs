091c4216a6a2387319fbbe5307f71bdad38ca303
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 08:01:20 +0330

    feat(event): add upcoming events endpoint

---
+++ src/EventFeedbackSystem.Web/EventFeedbackSystem.Web/Controllers/EventsController.cs
@@ -0,0 +1,30 @@
+using EventFeedbackSystem.Application.Shared.Auth;
+using EventFeedbackSystem.Application.Shared.Auth.Dtos;
+using EventFeedbackSystem.Application.Shared.Events;
+using EventFeedbackSystem.Application.Shared.Events.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventFeedbackSystem.Web.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class EventsController : ControllerBase
+{
+    private readonly IEventsService _eventsService;
+
+    public EventsController(IEventsService eventsService)
+    {
+        _eventsService = eventsService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUpcoming([FromQuery]GetUpcomingListInput input)
+    {
+        var result = await _eventsService.GetUpcomingList(input);
+
+        if (result.IsSuccess)
+            return Ok(result);
+
+        return BadRequest(result);
+    }
+}
