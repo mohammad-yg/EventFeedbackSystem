01c426ddf93f1abc599356fa268523847816ae1b
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 04:18:30 +0330

    refactor(EntityFrameworkCore): add some global usings

--- src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/usings.cs
+++ src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/usings.cs
@@ -1,2 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using EventFeedbackSystem.Core.Shared;
+global using EventFeedbackSystem.EntityFrameworkCore.Shared;
+global using EventFeedbackSystem.Shared;
