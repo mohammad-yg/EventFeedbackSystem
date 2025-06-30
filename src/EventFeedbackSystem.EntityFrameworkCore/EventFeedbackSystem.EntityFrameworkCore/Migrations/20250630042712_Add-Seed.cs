a35fb59e9829f1b15c4a923d4bcac8e0249ea60c
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 07:59:52 +0330

    feat(event): add seed

---
+++ src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/Migrations/20250630042712_Add-Seed.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EventFeedbackSystem.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "DateTime", "Description", "IsDeleted", "Location", "Title" },
+                values: new object[,]
+                {
+                    { 1L, new DateTime(2025, 6, 29, 4, 27, 12, 494, DateTimeKind.Utc).AddTicks(9742), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", false, "Tehran", "Event 1" },
+                    { 2L, new DateTime(2025, 7, 1, 4, 27, 12, 494, DateTimeKind.Utc).AddTicks(9750), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", false, "Tehran", "Event 2" },
+                    { 3L, new DateTime(2025, 7, 2, 4, 27, 12, 494, DateTimeKind.Utc).AddTicks(9752), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", false, "Tehran", "Event 3" },
+                    { 4L, new DateTime(2025, 7, 3, 4, 27, 12, 494, DateTimeKind.Utc).AddTicks(9753), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", false, "Tehran", "Event 4" },
+                    { 5L, new DateTime(2025, 7, 4, 4, 27, 12, 494, DateTimeKind.Utc).AddTicks(9754), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", false, "Tehran", "Event 5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 5L);
+        }
+    }
+}
--- src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/Shared/AppDbContext.cs
+++ src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/Shared/AppDbContext.cs
@@ -20,6 +20,49 @@ public class AppDbContext : DbContext
         // Apply configurations from the assembly
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 
+        modelBuilder.Entity<Event>().HasData(new Event[]
+        {
+            new Event
+            {
+                Id = 1,
+                Title = "Event 1",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                DateTime = DateTime.UtcNow - TimeSpan.FromDays(1),
+                Location = "Tehran"
+            },
+            new Event
+            {
+                Id = 2,
+                Title = "Event 2",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                DateTime = DateTime.UtcNow + TimeSpan.FromDays(1),
+                Location = "Tehran"
+            },
+            new Event
+            {
+                Id = 3,
+                Title = "Event 3",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                DateTime = DateTime.UtcNow + TimeSpan.FromDays(2),
+                Location = "Tehran"
+            },
+            new Event
+            {
+                Id = 4,
+                Title = "Event 4",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                DateTime = DateTime.UtcNow + TimeSpan.FromDays(3),
+                Location = "Tehran"
+            },
+            new Event
+            {
+                Id = 5,
+                Title = "Event 5",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                DateTime = DateTime.UtcNow + TimeSpan.FromDays(4),
+                Location = "Tehran"
+            },
+        });
 
         base.OnModelCreating(modelBuilder);
     }
