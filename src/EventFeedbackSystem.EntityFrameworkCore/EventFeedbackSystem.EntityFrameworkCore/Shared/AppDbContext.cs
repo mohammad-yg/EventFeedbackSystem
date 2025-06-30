6fdbaa081bbd59b1a197271c54b1573a4fd790ec
mohammad-yg <mohammadyadegari032@gmail.com>
2025-06-30 04:19:05 +0330

    feat(EntityFrameworkCore): add app db context

---
+++ src/EventFeedbackSystem.EntityFrameworkCore/EventFeedbackSystem.EntityFrameworkCore/Shared/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace EventFeedbackSystem.EntityFrameworkCore.Shared;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Apply configurations from the assembly
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
