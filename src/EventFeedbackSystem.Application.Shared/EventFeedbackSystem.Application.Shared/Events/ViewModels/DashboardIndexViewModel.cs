f34f5b5b1e0ba93f8b8a09e88f6794cd72ee38cd
mohammad-yg <mohammadyadegari032@gmail.com>
2025-07-02 00:28:57 +0330

    feat(dashboard): add dashboard index endpoint

---
+++ src/EventFeedbackSystem.Application.Shared/EventFeedbackSystem.Application.Shared/Events/ViewModels/DashboardIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿using EventFeedbackSystem.Application.Shared.Events.Dtos;
+
+namespace EventFeedbackSystem.Application.Shared.Events.ViewModels;
+
+public class DashboardIndexViewModel
+{
+    public IEnumerable<EventListOutput> RegisterdEvents { get; set; }
+}
---
+++ src/EventFeedbackSystem.Web/EventFeedbackSystem.Web/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using EventFeedbackSystem.Application.Shared.Events;
+using EventFeedbackSystem.Application.Shared.Events.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventFeedbackSystem.Web.Controllers;
+
+[Authorize]
+public class DashboardController : BaseController
+{
+    private readonly IEventsService _eventsService;
+
+    public DashboardController(IEventsService eventsService)
+    {
+        _eventsService = eventsService;
+    }
+
+    [HttpGet]
+    [ActionName("")]
+    public async Task<IActionResult> Index()
+    {
+        var currentUserId = GetCurrentUserId();
+        var result = await _eventsService.GetUserRegisterationsList(currentUserId);
+
+        if (result.IsSuccess) return Ok(new DashboardIndexViewModel()
+        {
+            RegisterdEvents = result.Data
+        });
+
+        return BadRequest(result);
+    }
+}
