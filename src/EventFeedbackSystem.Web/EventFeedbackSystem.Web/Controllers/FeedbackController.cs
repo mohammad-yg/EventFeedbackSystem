79f7ee73e8355dbd5fd451c686d8fb9a977a6791
mohammad-yg <mohammadyadegari032@gmail.com>
2025-07-02 01:47:29 +0330

    feat(feedback): add submit feedback endpoint

---
+++ src/EventFeedbackSystem.Web/EventFeedbackSystem.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,30 @@
+﻿using EventFeedbackSystem.Application.Shared.Auth.Dtos;
+using EventFeedbackSystem.Application.Shared.Events;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventFeedbackSystem.Web.Controllers;
+
+public class FeedbackController : BaseController
+{
+    private readonly IFeedbackService _feedbackService;
+
+    public FeedbackController(IFeedbackService feedbackService)
+    {
+        _feedbackService = feedbackService;
+    }
+
+    [HttpPost]
+    [ActionName("{eventId}")]
+    [Authorize]
+    public async Task<IActionResult> Submit([FromRoute] long eventId, [FromBody] AddFeedbackInput input)
+    {
+        var userId = GetCurrentUserId();
+        var result = await _feedbackService.AddFeedbackAsync(userId, eventId, input);
+
+        if (result.IsSuccess)
+            return Ok(result);
+
+        return BadRequest(result);
+    }
+}
